7944ce45ee9a84a1c52866a00dab7dd624d3a75a
= <=>
2018-04-02 23:53:35 +0200

    #? added slider with images

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,8 +2,12 @@
 	<div class="slider-content">
 		<slider animation="fade">
 		  <slider-item v-for="(image, index) in list" :key="index">
-		    <div>
+		    <div class="image-container">
 		      <img class="mySlides" :src="image.src">
+		      <div class="centered">
+		      	<h2>{{image.title}}</h2>
+		      	<p>{{image.content}}</p>
+		      </div>
 		    </div>
 		  </slider-item>
 		</slider>
@@ -17,7 +21,19 @@ export default {
 		return {
 			"list" : [
 				{
-					"src" : "../../img/code.jpg"
+					"src" : "../../img/code.jpg",
+					"title" : "Software Developer",
+					"content" : "I have a large interest in programming and I love to apply my skills to my personal projects. Fond of data engineering, full stack development and machine learning" 
+				},
+				{
+					"src" : "../../img/calcio.jpg",
+					"title" : "Sport fun",
+					"content" : "I played football since I was 5 years old to 18. Actually I'm a follower of football, tennis and basket"
+				},
+				{
+					"src" : "../../img/collage.png",
+					"title" : "Life lover",
+					"content" : "I spend my free time with my friends, that are really my life. I like going out to know new people, drink gin tonics and have fun"
 				}
 			]
 		}
@@ -30,14 +46,28 @@ export default {
 	
 </script>
 <style lang="scss">
+
+	@media (min-width: 700px) {
+	    .slider {
+			height: 502px !important;
+		}
+	}
 	
 	@media (max-width: 700px) {
 	    .mySlides {
-			height: 250px !important;
+			height: 350px !important;
 		}
 
 		.slider {
-			height: 250px;
+			height: 350px !important;
+		}
+
+		.centered h2 {
+			font-size: 38px !important;
+		}
+
+		.centered p {
+			font-size: 10px !important;
 		}
 	}
 
@@ -47,12 +77,54 @@ export default {
 		}
 
 		.slider {
-			height: 150px;
+			height: 150px !important;
+		}
+
+		.centered h2 {
+			font-size: 28px important;
+		}
+
+		.centered p {
+			font-size: 10px important;
 		}
 	}
 
 	.mySlides {
 		width:100%;
-		height: 302px;
+		height: 502px;
+	}
+
+	.container {
+	    position: relative;
+	    text-align: center;
+	    color: white;
+	}
+
+	.centered {
+	    position: absolute;
+	    width: 100%;
+	    top: 70%;
+	    left: 50%;
+	    transform: translate(-50%, -50%);
+	}
+
+	.centered h2 {
+		font-size: 48px;
+	    text-transform: uppercase;
+	    font-weight: 800;
+	    margin-bottom: 30px;
+	    color: white;
+	    text-align: center;
+	}
+
+	.centered p {
+		font-size: 16px;
+	    text-transform: uppercase;
+	    letter-spacing: 2px;
+	    width: 50%;
+	    margin: 0 auto 30px auto;
+	    line-height: 1.8em;
+	    color: white;
+	    text-align: center;
 	}
 </style>
